c1d573b0ed0d60ea83021aa300a0d6db2cffa02b
Teng-404 <66200341@Kmitl.ac.th>
2026-04-23 11:30:21 +0700

    update loan

--- obj/Debug/net9.0/POOC.AssemblyInfo.cs
+++ obj/Debug/net9.0/POOC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325e70a407e013254e696f480f2821c2b85d69c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("POOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
