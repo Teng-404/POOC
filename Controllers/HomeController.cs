a201b1a77f1f60a60e4d59decad73e28129f3a22
Teng-404 <66200341@Kmitl.ac.th>
2026-04-28 08:50:23 +0700

    fix project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using POOC.Models;
 public class HomeController : Controller
 {
     private readonly ApplicationDbContext _context;
-    private static List<string> _history = new();
     public HomeController(ApplicationDbContext context)
     {
         _context = context;
@@ -16,49 +15,6 @@ public class HomeController : Controller
             Members = _context.Members.ToList()
         };
 
-            return View("Member", vm); // ไป Views/Home/Member.cshtml
-    }
-    public IActionResult Index()
-    {
-        ViewBag.History = _history;
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult Index(double? width, double? length, double? radius, double? coneRadius, double? coneHeight)
-    {
-        if (width.HasValue && length.HasValue)
-        {
-            var result = width.Value * length.Value;
-            ViewBag.SquareResult = result;
-            _history.Add($"Square: {result}");
-        }
-
-        if (radius.HasValue)
-        {
-            var result = Math.PI * Math.Pow(radius.Value, 2);
-            ViewBag.CircleResult = result;
-            _history.Add($"Circle: {result}");
-        }
-
-        if (coneRadius.HasValue && coneHeight.HasValue)
-        {
-            double r = coneRadius.Value;
-            double h = coneHeight.Value;
-
-            var result = Math.PI * r * (r + Math.Sqrt(h * h + r * r));
-            ViewBag.ConeResult = result;
-            _history.Add($"Cone: {result}");
-        }
-
-        ViewBag.History = _history;
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult ClearHistory()
-    {
-        _history.Clear();
-        return RedirectToAction("Index");
+            return View("Member", vm); 
     }
 }
