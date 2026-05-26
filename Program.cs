308051ee07d4b9746293e4169496b53bc5c1296f
Teng-404 <66200341@Kmitl.ac.th>
2026-05-26 15:42:50 +0700

    Remove manual port binding

--- Program.cs
+++ Program.cs
@@ -120,9 +120,4 @@ using (var scope = app.Services.CreateScope())
     context.SaveChanges();
 }
 
-var port = Environment.GetEnvironmentVariable("ASPNETCORE_PORT") 
-           ?? Environment.GetEnvironmentVariable("PORT") 
-           ?? "8080";
-app.Urls.Add($"http://0.0.0.0:{port}");
-
 app.Run();
